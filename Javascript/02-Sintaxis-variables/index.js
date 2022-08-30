509a15439403a613a92ec443d6537c1a530cff47
Carlos Daniel Ortega Hernandez <carlos.ortega.ast0318@gmail.com>
2022-08-30 13:09:56 -0500

    feat: add a new list with user information

---
+++ Javascript/02-Sintaxis-variables/index.js
@@ -0,0 +1,12 @@
+const info = [
+    'Carlos',
+    24,
+    true,
+    new Date(1998, 6, 25),
+    {
+        title: 'El fin de la eternindad',
+        autor: 'Isaac Asimov',
+        fecha: new Date(2019, 10, 01),
+        url: 'https://www.amazon.com.mx/El-fin-eternidad-Isaac-Asimov/dp/6073159269'
+    }
+];
