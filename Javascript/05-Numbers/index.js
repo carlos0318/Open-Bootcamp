f36bd1a4581ead250c6af3683a65fc57a6f161eb
Carlos Daniel Ortega Hernandez <carlos.ortega.ast0318@gmail.com>
2022-08-31 14:15:12 -0500

    feat: numbers in Javascript

---
+++ Javascript/05-Numbers/index.js
@@ -0,0 +1,11 @@
+let heightCm = 155;
+let heightM = 1.55;
+let weigthKg = 64.3;
+
+let height = Math.ceil(heightM);
+console.log(height);
+let weigth = Math.floor(weigthKg);
+console.log(weigth);
+
+let isEqual = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(isEqual);
