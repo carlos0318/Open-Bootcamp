49f3e5bd2b5807dded314e44ac7a478779d36500
Carlos Daniel Ortega Hernandez <carlos.ortega.ast0318@gmail.com>
2022-09-03 20:28:42 -0500

    feat: add jquery and events to the project

---
+++ Javascript/14-js-html/index.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    alert("click en el botón")
+});
+
+$('button').click(function(){
+    console.log('Hola, estoy utilizando jQuery');
+});
